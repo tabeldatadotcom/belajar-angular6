db4be7df27f3d3a6d6e2265fd2fbbfd6fddc8396
dimMaryanto@win10 <software.dimmaryanto@hotmail.com>
2018-05-11 15:08:41 +0700

    update membuat form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,15 @@ export class AppComponent {
   // ini komentar
   title: string = 'Aplikasi Perpustakaan';
   books: string[] = ['Java', 'PHP', 'Javascript', 'Ruby', 'Swift', 'Phyton', 'Scala'];
+
+  book: Book = new Book();
+
+  kirimData(data){
+    console.log(this.book);
+  }
+
+}
+
+export class Book{
+  public name: string;
 }
